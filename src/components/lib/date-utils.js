f15d165e13f5e2702f61c407b79a4f826175279a
cedrtang <cedric@beyowi.com>
2022-08-29 14:48:57 -0700

    fix(plugins): add js extension for sveltekit import

--- src/components/lib/date-utils.js
+++ src/components/lib/date-utils.js
@@ -1,14 +1,12 @@
-import dayjs from 'dayjs'
-import localeData from 'dayjs/plugin/localeData'
-import minMax from 'dayjs/plugin/minMax'
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
-import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData.js';
+import minMax from 'dayjs/plugin/minMax.js';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore.js';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter.js';
 
-dayjs.extend(localeData)
-dayjs.extend(minMax)
-dayjs.extend(isSameOrBefore)
-dayjs.extend(isSameOrAfter)
+dayjs.extend(localeData);
+dayjs.extend(minMax);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
 
-export {
-  dayjs
-}
+export { dayjs };
