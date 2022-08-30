0b490912c77c30aa427594553537e3f4918620e6
cedrtang <cedric@beyowi.com>
2022-08-30 15:01:03 -0700

    feat(locale): add french locale

--- src/components/lib/date-utils.js
+++ src/components/lib/date-utils.js
@@ -3,10 +3,12 @@ import localeData from 'dayjs/plugin/localeData.js';
 import minMax from 'dayjs/plugin/minMax.js';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore.js';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter.js';
+import 'dayjs/locale/fr';
 
 dayjs.extend(localeData);
 dayjs.extend(minMax);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(isSameOrAfter);
+dayjs.locale('fr');
 
 export { dayjs };
