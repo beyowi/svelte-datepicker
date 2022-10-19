dded1472c42a499a334231f649eb6a33522bb6f4
cedrtang <cedric@beyowi.com>
2022-10-19 14:02:04 -0700

    fix(locale) bug of 1 day diff when using locale fr, use fr-ca

--- src/components/lib/date-utils.js
+++ src/components/lib/date-utils.js
@@ -3,12 +3,12 @@ import localeData from 'dayjs/plugin/localeData.js';
 import minMax from 'dayjs/plugin/minMax.js';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore.js';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter.js';
-import 'dayjs/locale/fr';
+import 'dayjs/locale/fr-ca';
 
 dayjs.extend(localeData);
 dayjs.extend(minMax);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(isSameOrAfter);
-dayjs.locale('fr');
+dayjs.locale('fr-ca');
 
 export { dayjs };
